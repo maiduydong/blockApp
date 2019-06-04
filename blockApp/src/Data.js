79482664604e6f4ad680f2450d817b8fb31cd18e
Dong <maiduydong94@gmail.com>
2019-06-04 19:54:30 +0700

    j

--- blockApp/src/Data.js
+++ blockApp/src/Data.js
@@ -3,7 +3,8 @@ var Topic = {
     decription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam ",
     date: "Sunday: 12pm ",
     avatar: require ('../res/CanhKy.png'),
-    author:"Erick Smith "
+    author:"Erick Smith ",
+    sumVisit: 117
     
 }
 export default Topic
--- blockApp/src/Main.js
+++ blockApp/src/Main.js
@@ -22,46 +22,48 @@ export default class Main extends Component {
                 <View style={styles.content}>
 
 
-                   
-                        <ImageBackground source = {IMG.BOOK} style={styles.img}>
-                            <View style= {styles.icon}>
-                                <Text style = {styles.text}>LIFESTYLE</Text>
-                             
-                                 <View style = {styles.name}>
 
-                                     <Image  source ={Topic.avatar}/>
-                                     <View style = {styles.authorName}>
+                    <ImageBackground source={IMG.BOOK} style={styles.img}>
+                        <View style={styles.img}>
+                            <View style={styles.nhandan}>
+                                <Text style={styles.textnhandan}>LIFESTYLE</Text>
+                            </View>
+                            <View style={styles.authorinimage}>
+                                <Image source={Topic.avatar} />
+                                <View>
+                                <Text style={styles.textAuthor}>{Topic.author}</Text>
+                                <Text style={styles.textDate}>{Topic.date}</Text>
+                                </View>
+                                
 
-                                         <Text style = {styles.authorText}>{Topic.author}</Text>
-                                         <Text style = {styles.dateText}>{Topic.date}</Text>
-                                     </View>
+                            </View>
+                        </View>
 
-                                 </View>
-                            </View> 
-                            
-                        </ImageBackground>
-                    
+
+
+                    </ImageBackground>
+
 
 
                     <View style={styles.note}>
-                        <View style = { styles.titleCon}> 
-                            <Text style = {styles.title}> {Topic.title}</Text>
+                        <View style={styles.titleCon}>
+                            <Text style={styles.title}> {Topic.title}</Text>
                         </View>
-                        <View style = {styles.dersiptionCon}>
-                            <Text style = {styles.dersiption}>{Topic.decription}</Text>
+                        <View style={styles.dersiptionCon}>
+                            <Text style={styles.dersiption}>{Topic.decription}</Text>
                         </View>
-                        <View style = {{flexDirection:'row'}}>
-                            <View style ={{flexDirection:'row',alignItems:'flex-start', justifyContent:'flex-start'}}>
-                                     <Image style ={{resizeMode:'cover', marginBottom: 50,}} source ={require('../res/like.png')}/>
-                                     <Image style ={{ resizeMode:'center', marginRight: 200  }}source ={require('../res/comments.png')}></Image>
+                        <View style={styles.bottom}>
+                            <View style = {styles.bottomLeft}>
+                                <Image  style ={{marginTop: 1,height: 50, width: 50, resizeMode:'contain'}} source= {require('../res/like.png')}></Image>
+                                <Image style ={{marginTop: 1,height: 50, width: 50,resizeMode:'center'}} source={require('../res/comments.png')}></Image>
                             </View>
-                            <View style ={{flexDirection:'column',alignItems:'flex-start', justifyContent:'flex-start'}} >
-                                    <Image  source ={Topic.avatar}/>
-                                    <Image  source ={Topic.avatar}/>
-                                    <Text> 117</Text>
-                            </View>
+                            <View style ={{width: width - 220}}/>
+                            <View style ={styles.bottomRight}>
+                                <Image style ={styles.bottomRightImage} source= {require('../res/DiNang.png')}/>
+                                <Text style={{paddingLeft: 1, color:'#828282', fontSize:20 }}>{`+`+Topic.sumVisit}</Text>
+                            
 
-                           
+                            </View>
                         </View>
                     </View>
 
@@ -79,95 +81,134 @@ export default class Main extends Component {
 const styles = StyleSheet.create({
     container: {
 
-        flexDirection: 'row',
+        flexDirection: 'column',
         backgroundColor: '#E5E5E5',
         justifyContent: 'center',
         alignItems: 'center',
         paddingTop: 50,
     },
     content: {
-        
+
         width: width - 40,
-        height: height * 3/ 4,
+        height: height * 3 / 4,
+        flexDirection:'column',
         backgroundColor: '#E5E5E5',
-        justifyContent: 'center',
-        alignItems: 'center',
+        justifyContent: 'flex-start',
+        alignItems: 'flex-start',
         borderTopLeftRadius: 10,
         borderTopRightRadius: 10,
-        
+
     },
     img: {
 
-        flexDirection: 'row',
+        flexDirection: 'column',
         borderTopLeftRadius: 10,
-        backgroundColor: 'red',
+        justifyContent: 'space-between',
+
         width: width - 50,
         height: height * 0.4
 
     },
+    nhandan: {
+        width: 100,
+        height: 30,
+        borderRadius: 10,
+        backgroundColor: '#F9A825',
+        marginTop: 10,
+        marginLeft: 10,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    textnhandan: {
+        color: 'white',
+        fontSize: 15
+    },
+    authorinimage: {
+        paddingBottom: 4,
+        flexDirection: 'row',
+        paddingLeft: 5
+    },
+    textAuthor:{
+        color:'#FFFFFF',
+        fontSize: 15,
+        fontWeight:'bold',
+        paddingLeft: 5,
+    },
+    textDate:{
+        color:'#FFFFFF',
+        fontSize: 12,
+        paddingLeft: 5,
+    },
     note: {
 
+
+        width: width - 100,
+        height: (height * 3 / 4) - (height * 0.4),
+        flexDirection:'column',
        
-        width: width - 50,
-        height: (height * 2 / 3) - (height * 0.40)
 
     },
-    icon:{
+    icon: {
         width: 100,
         height: 25,
         borderRadius: 10,
         backgroundColor: '#F9A825',
         marginTop: 10,
         marginLeft: 5,
-        justifyContent:'center',
+        justifyContent: 'center',
         alignItems: 'center',
     },
-    text:{
-        color:'white',
+    text: {
+        color: 'white',
         fontSize: 15,
-        marginTop:10
-        
+        marginTop: 10
+
     },
-    titleCon:{
-        width: width -40,
+    titleCon: {
+        width: width - 45,
         height: 70,
-       
+
     },
-    dersiptionCon:{
-        width: width - 40,
-        height:100,
-      
+    dersiptionCon: {
+        width: width - 50,
+        height: 100,
+
     },
-    title:{
-        fontSize:20,
-        paddingTop:30,
+    title: {
+        fontSize: 20,
+        paddingTop: 30,
         fontWeight: 'bold',
     },
-    dersiption:{
+    dersiption: {
         fontSize: 15,
         opacity: 0.7,
     },
-    name:{
-        width: 200,
-        height: 50,
-        
-        justifyContent:'flex-end',
-        marginTop: 500,
-        flexDirection: 'row',
-        marginLeft: 20,
-        
+    bottom:{
+        flexDirection:'row',
+        backgroundColor: '#E5E5E5',
+        //width: width - 50,
+        height: 100,
+        justifyContent: 'space-between',
+       
     },
-    authorName:{
-        flexDirection: 'row',
-        width: 80,
+    bottomLeft:{
+        flexDirection:'row',
+        justifyContent:'flex-start',
+       
+    },
+    bottomRight:{
+        flexDirection:'row',
+        paddingRight: 10,
+        marginTop: 10,
+        justifyContent:'space-between'
+    },
+    bottomRightImage:{
+        width:20,
+        height:20,
+        resizeMode:'contain',
         marginRight: 10,
-    },
-    authorText:{
-        fontSize: 20,
-        color:"white"
-    },
-    dateText:{
-        fontSize: 15,
-        color:"white"
     }
+
+
+
 })
